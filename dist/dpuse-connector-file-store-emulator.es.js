8e07371986c8a47152409bda9dfa58f6c0811ac5
Jonathan Terrell <terrell.jm@gmail.com>
2026-04-29 18:35:12 +0200

    v0.2.489

--- dist/dpuse-connector-file-store-emulator.es.js
+++ dist/dpuse-connector-file-store-emulator.es.js
@@ -103,7 +103,7 @@ var m = [
 		authMethodId: "none",
 		maxConnectionCount: 1
 	} },
-	icon: "<svg viewBox=\"0 0 24 24\" fill=\"none\" stroke-width=\"1\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-swatch-book-icon lucide-swatch-book\"><path d=\"M16.7 13H19a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H7\" style=\"fill: rgb(47, 141, 130); stroke: rgb(47, 141, 130);\"/><path d=\"m11 8 2.3-2.3a2.4 2.4 0 0 1 3.404.004L18.6 7.6a2.4 2.4 0 0 1 .026 3.434L9.9 19.8\" style=\"stroke: rgb(200, 149, 56); fill: rgb(200, 149, 56);\"/><path d=\"M11 17a4 4 0 0 1-8 0V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2Z\" style=\"fill: rgb(77, 131, 224); stroke: rgb(77, 131, 224);\"/><path d=\"M 7 17h.01\"/></svg>",
+	icon: "<svg viewBox=\"0 0 24 24\" fill=\"none\" stroke-width=\"1\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-swatch-book-icon lucide-swatch-book\"><path d=\"M16.7 13H19a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H7\" style=\"fill: rgb(47, 141, 130); stroke: rgb(47, 141, 130);\"/><path d=\"m11 8 2.3-2.3a2.4 2.4 0 0 1 3.404.004L18.6 7.6a2.4 2.4 0 0 1 .026 3.434L9.9 19.8\" style=\"stroke: rgb(200, 149, 56); fill: rgb(200, 149, 56);\"/><path d=\"M11 17a4 4 0 0 1-8 0V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2Z\" style=\"fill: rgb(77, 131, 224); stroke: rgb(77, 131, 224);\"/><path d=\"M 7 17h.01\" style=\"stroke: rgb(255, 255, 255);\"/></svg>",
 	iconDark: "",
 	lastUpdatedAt: null,
 	operations: [
@@ -122,7 +122,7 @@ var m = [
 	vendorAccountURL: null,
 	vendorDocumentationURL: null,
 	vendorHomeURL: null,
-	version: "0.2.488"
+	version: "0.2.489"
 }, g = {
 	"": [
 		{
